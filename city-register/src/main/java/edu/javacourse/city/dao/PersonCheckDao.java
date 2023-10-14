262e56861b91b68df8f6d991bc457de126db4c33
Greem4 <i@greem4.ru>
2023-10-14 21:16:43 +0300

    R 87

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -13,32 +13,47 @@ public class PersonCheckDao {
             "select temporal from cr_address_person ap " +
                     "         inner join cr_person p on p.person_id = ap.person_id " +
                     "         inner join cr_address a on a.address_id = ap.address_id " +
-                    "where upper(p.sur_name COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") " +
+                    "where " +
+                    "CURRENT_DATE >= ap.start_date and (CURRENT_DATE <= ap.end_data or ap.end_data is null) " +
+                    "  and upper(p.sur_name COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") " +
                     "  and upper(p.given_name) = upper(? COLLATE \"ru_RU.utf8\")" +
                     "  and upper(patronymic COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") " +
-                    "  and p.date_of_birth = ?" +
-                    "  and a.street_code = ?" +
-                    "  and upper(a.building COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") " +
-                    "  and upper(extension COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") " +
-                    "  and upper(a.apartment COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") " ;
-
+                    "  and p.date_of_birth = ? " +
+                    "  and a.street_code = ? " +
+                    "  and upper(a.building COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") ";
 
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
 
+        String sql = SQL_REQUEST;
+        if (request.getExtension() != null) {
+            sql += " and upper(a.extension COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") ";
+        } else {
+            sql += "and extension is null ";
+        }
+        if (request.getApartment() != null) {
+            sql += " and upper(a.apartment COLLATE \"ru_RU.utf8\") = upper(? COLLATE \"ru_RU.utf8\") ";
+        } else {
+            sql += "and a.apartment is null ";
+        }
 
         try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(SQL_REQUEST)) {
+             PreparedStatement stmt = con.prepareStatement(sql)) {
 
-            stmt.setString(1, request.getSurName());
-            stmt.setString(2, request.getGivenName());
-            stmt.setString(3, request.getPatronymic());
-            stmt.setDate(4, java.sql.Date.valueOf(request.getDateOfBirth()));
-            stmt.setInt(5, request.getStreetCode());
-            stmt.setString(6, request.getBuilding());
-            stmt.setString(7, request.getExtension());
-            stmt.setString(8,request.getApartment());
+            int count = 1;
+            stmt.setString(count++, request.getSurName());
+            stmt.setString(count++, request.getGivenName());
+            stmt.setString(count++, request.getPatronymic());
+            stmt.setDate(count++, java.sql.Date.valueOf(request.getDateOfBirth()));
+            stmt.setInt(count++, request.getStreetCode());
+            stmt.setString(count++, request.getBuilding());
+            if (request.getExtension() != null) {
+                stmt.setString(count++, request.getExtension());
+            }
+            if (request.getApartment() != null) {
+                stmt.setString(count++, request.getApartment());
+            }
 
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {

64e1b865dc89fafca851882587ec3e2b11596bfe
Greem4 <i@greem4.ru>
2023-10-16 22:16:53 +0300

    R 90, multithread

--- network/src/main/java/edu/javacourse/net/Server.java
+++ network/src/main/java/edu/javacourse/net/Server.java
@@ -6,32 +6,50 @@ import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(25225);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket socket = new ServerSocket(25225, 200);
 
         System.out.println("Server is started");
         while (true) {
             Socket client = socket.accept();
-            handleRequest(client);
+            new SimpleServer(client).start();
         }
     }
 
-    private static void handleRequest(Socket client) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-
-        StringBuilder sb = new StringBuilder("Hello, ");
-        String userName = br.readLine();
-        System.out.println("Server got string: " + userName);
-
-        sb.append(userName);
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.flush();
-
-        br.close();
-        bw.close();
-
-        client.close();
+}
+class SimpleServer extends Thread    {
+    private Socket client;
+    public SimpleServer(Socket client) {
+        this.client = client;
     }
+    @Override
+    public void run() {
+        handleRequest();
+    }
+
+    private void handleRequest() {
+        try {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            StringBuilder sb = new StringBuilder("Hello, ");
+            String userName = br.readLine();
+            System.out.println("Server got string: " + userName);
+            Thread.sleep(2000);
+
+            sb.append(userName);
+            bw.write(sb.toString());
+            bw.newLine();
+            bw.flush();
+
+            br.close();
+            bw.close();
+
+            client.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }

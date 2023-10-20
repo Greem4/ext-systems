22cc35d12b76a7187b1a3cc2d21a45ec3cc61351
Greem4 <i@greem4.ru>
2023-10-20 17:08:27 +0300

    R 98, simple HTTP request processing in the application V2.

--- city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
@@ -1,12 +1,12 @@
 package edu.javacourse.city.web;
 
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet(name = "CheckPersonServlet", urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet {

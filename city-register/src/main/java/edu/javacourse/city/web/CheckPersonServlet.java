42c4825a7c8182a6da5627605d5cb1c8611a04ee
Greem4 <i@greem4.ru>
2023-10-20 12:02:06 +0300

    R 98, simple HTTP request processing in the application.

---
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
@@ -0,0 +1,18 @@
+package edu.javacourse.city.web;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet(name = "CheckPersonServlet", urlPatterns = {"/checkPerson"})
+public class CheckPersonServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Get CheckPerson - called");
+    }
+}
